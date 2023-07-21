f7fe7a1c7939aaad25026b0e10f645724605fea9
medvedevsi <medvedevsi@bk.ru>
2023-07-22 00:47:06 +0300

    add book

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Book book = new Book("Ulysses", "James Joys", 600, "3456GH");
         System.out.println("Bookname: " + book.getAuthor() + " " + book.getName());
+        Book book = new Book("Fifth Heart", "Dan Simmons", 500, "11GH89H");
+        System.out.println("Bookname: " + book.getAuthor() + " " + book.getName());
     }
 }
