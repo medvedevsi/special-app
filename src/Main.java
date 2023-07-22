b3ae92cbe5508c66c0c799d18596a573c7df0bde
medvedevsi <64162608+medvedevsi@users.noreply.github.com>
2023-07-22 16:27:11 +0300

    Update Main.java errorcorrection and new book

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("Ulysses", "James Joys", 600, "3456GH");
-        System.out.println("Bookname: " + book.getAuthor() + " " + book.getName());
-        Book book = new Book("Fifth Heart", "Dan Simmons", 500, "11GH89H");
-        System.out.println("Bookname: " + book.getAuthor() + " " + book.getName());
-        Book book = new Book("Путешествие Алисы", "Кир Булычев", 211, "1WV4566");
-        System.out.println("Bookname: " + book.getAuthor() + " " + book.getName());
-        Book book = new Book("Приключения Тома Сойера", "Марк Твен", 349, "FFG1234H");
-        System.out.println("Bookname: " + book.getAuthor() + " " + book.getName());
+        Book Ulysses = new Book("Ulysses", "James Joys", 600, "3456GH");
+        System.out.println("Bookname: " + Ulysses.getAuthor() + " " + Ulysses.getName());
+        Book FHeart = new Book("Fifth Heart", "Dan Simmons", 500, "11GH89H");
+        System.out.println("Bookname: " + FHeart.getAuthor() + " " + FHeart.getName());
+        Book Alice = new Book("Путешествие Алисы", "Кир Булычев", 211, "1WV4566");
+        System.out.println("Bookname: " + Alice.getAuthor() + " " + Alice.getName());
+        Book Tom = new Book("Приключения Тома Сойера", "Марк Твен", 349, "FFG1234H");
+        System.out.println("Bookname: " + Tom.getAuthor() + " " + Tom.getName());
+        Book Gomer = new Book("Илиада и Одиссея", "Гомер", 349, "NNM45fh7");
+        System.out.println("Bookname: " + Gomer.getAuthor() + " " + Gomer.getName());
     }
 }
